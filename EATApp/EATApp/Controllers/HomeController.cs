63bb61b6072231290943349bb6903f53ef56fce9
Steven <mcgevo@gmail.com>
2020-06-05 11:27:05 +0930

    Linked Database to project Added LoginView.cshtml and SignInView.cshtml

--- EATApp/EATApp/Controllers/HomeController.cs
+++ EATApp/EATApp/Controllers/HomeController.cs
@@ -8,24 +8,7 @@ namespace EATApp.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+        
 
         public ActionResult ClassView()
         {
